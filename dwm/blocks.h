816639723533ae7a1cef4b6466609fa33790ee32
TWM Linux - Gabriel <90172365+GabrielTWMlinux@users.noreply.github.com>
2022-03-30 23:17:15 -0300

    Add files via upload

---
+++ dwm/blocks.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"   ", "~/Scripts/volumedwm",						1,		10},
+
+	{"   ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
+
+	{"  ", "date '+%d %b %Y, %a %H:%M '",					60,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
