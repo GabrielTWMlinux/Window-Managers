1440ef3ab01bde8188ccdeda35ab8e5b5591a5ed
TWM Linux - Gabriel <90172365+GabrielTWMlinux@users.noreply.github.com>
2022-03-30 22:47:57 -0300

    Add files via upload

---
+++ dwm/autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dwmblocks &
+picom --experimental-backends &
+nitrogen --restore &
+numlockx on &
+polkit-xfce-authentication-agent-1 &
+xset r rate 300 50 &
+xset s off &
+xset -dpms &
