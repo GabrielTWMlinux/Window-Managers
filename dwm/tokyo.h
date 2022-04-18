b57ee4842dc9ad4c5e57b07c4428bddfbbcf9a12
TWM Linux - Gabriel <90172365+GabrielTWMlinux@users.noreply.github.com>
2022-04-18 18:12:28 -0300

    Add files via upload

---
+++ dwm/tokyo.h
@@ -0,0 +1,5 @@
+static const char col_gray1[]       = "#111118";
+static const char col_gray2[]       = "#000000";
+static const char col_gray3[]       = "#D0D0D0";
+static const char col_gray4[]       = "#FFFFFF";
+static const char col_gray5[]       = "#345e81";
