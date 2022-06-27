9dfb4bb7f791373bdca4f9b8a0b20c60de23ec0f
TWM Linux - Gabriel <90172365+GabrielTWMlinux@users.noreply.github.com>
2022-06-27 00:23:59 -0300

    Add files via upload

--- dwm/dwmblocks/blocks.h
+++ dwm/dwmblocks/blocks.h
@@ -1,15 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ", "~/Scripts/Void-Updates",		3600,		10},
+	/*Icon*/	/*Command*/			/*Update Interval*/	/*Update Signal*/
+	{"  ", "~/.config/Scripts/Void-Updates",		3600,		10},
 
-	{"  ", "~/Scripts/volumedwm",			1,		0},
+	{"  ", "~/.config/Scripts/weather",			3600,		0},
 
-	{"  ", "~/Scripts/ram-dwm",			5,		0},
+	{"  ", "~/.config/Scripts/volumedwm",			1,		0},
+
+	{"  ", "~/.config/Scripts/ram-dwm",			5,		0},
 	
-	{"  ", "~/Scripts/cpu-dwm",			5,		0},
+	{"  ", "~/.config/Scripts/cpu-dwm",			5,		0},
 
-	{"  ", "date '+%d %b %Y, %a %H:%M '",		30,		0},
+	{"  ", "date '+%d %b %Y, %a %H:%M '",			30,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
