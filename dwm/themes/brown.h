4efdc73aed1ee3204ace6b781fae2844a1773a2d
TWM Linux - Gabriel <90172365+GabrielTWMlinux@users.noreply.github.com>
2022-07-05 20:07:11 -0300

    Add files via upload

--- dwm/themes/brown.h
+++ dwm/themes/brown.h
@@ -1,5 +1,5 @@
-static const char col_gray1[]       = "#080808";
+static const char col_gray1[]       = "#0b0b0f";
 static const char col_gray2[]       = "#000000";
-static const char col_gray3[]       = "#D0D0D0";
+static const char col_gray3[]       = "#d7d7d7";
 static const char col_gray4[]       = "#FFFFFF";
-static const char col_gray5[]       = "#9E5547";
+static const char col_gray5[]       = "#924441";
